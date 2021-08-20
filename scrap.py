1cda495c813dd06ba782b496839cef0882949a12
avdhut08 <45819476+avdhut08@users.noreply.github.com>
2021-08-20 17:06:11 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -8,10 +8,10 @@ class ScrapArticle:
         html_data = urlopen(url).read()
         soup_data = bs.BeautifulSoup(html_data, 'html.parser')
         nav = soup_data.nav
-        for nav_url in nav.find_all('a', text='Sport'):
+        for nav_url in nav.find_all('a', text='environmental'):
             category_data = urlopen(nav_url.get('href')).read()
             category_soup_data = bs.BeautifulSoup(category_data, 'html.parser')
-            for div in category_soup_data.find_all('div', string=re.compile('PSG|Pedro', re.IGNORECASE)):
+            for div in category_soup_data.find_all('div', string=re.compile('epidemic|corona virus', re.IGNORECASE)):
                 for article_link in div.find_all('a'):
                     article_data = urlopen(url + article_link.get('href')).read()
                     article_soup_data = bs.BeautifulSoup(article_data, 'html.parser')
